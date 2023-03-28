569bcbbef4f7fddc1b27a2530a6298ba2823ca6f
John Lai <lai.john.py@gmail.com>
2023-03-28 17:35:31 -0400

    Add button to look at my future

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,13 +6,16 @@ import React from 'react';
 const Home = () => {
   return (
     <main className="container mx-auto  py-6">
-      <section>
+      <section className="text-center mb-3">
         {/* <h3 class="animate__animated animate__bounce">An animated element</h3> */}
         <img
           className="max-w-full w-96 mx-auto rolate-ball"
           src="/src/img/site/crystal-ball-drawing-fortune-telling.png"
           alt="Crystal Ball"
         />
+        <button className="bg-fuchsia-500 hover:bg-fuchsia-400 text-white font-bold py-2 px-4 border-b-4 border-fuchsia-700 hover:border-fuchsia-500 rounded mt-6">
+          Look at my future
+        </button>
       </section>
       {/* <CategoryMenu /> */}
       {/* <ProductList /> */}
