e0d0df2dfb95e288a9feb15abe1f16919f610acd
John Lai <lai.john.py@gmail.com>
2023-03-28 15:09:49 -0400

    Add login btn form style

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -61,7 +61,11 @@ function Login(props) {
           </div>
         ) : null}
         <div className="flex flex-end">
-          <button type="submit">Submit</button>
+          <button
+            className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
