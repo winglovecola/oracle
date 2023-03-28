3f8ae72e54e43a2464f9b1f620e43fc0ecf7af2f
winglovecola@gmail.com <winglovecola@gmail.com>
2023-03-28 13:32:43 -0400

    updated Tarot Database and tarot card images

---
+++ server/models/Tarot.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const tarotSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  nameShort: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  value: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  valueInt: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  suit: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  type: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  meaningUp: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  meaningRev: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  desc: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+
+const Tarot = mongoose.model('Tarot', tarotSchema);
+
+module.exports = Tarot;
