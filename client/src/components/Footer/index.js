0282187bc84e8479a4c0cdd4b0423f698b08a279
John Lai <lai.john.py@gmail.com>
2023-03-28 14:23:49 -0400

    Add footer component

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className="bg-green-300">
+      <h6>TEST</h6>
+    </div>
+  );
+}
+
+export default Footer;
