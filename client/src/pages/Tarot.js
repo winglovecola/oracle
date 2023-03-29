575897fba34def613da0ebb06621387a91867d01
John Lai <lai.john.py@gmail.com>
2023-03-28 23:06:57 -0400

    Add tarot card page

--- client/src/pages/Tarot.js
+++ client/src/pages/Tarot.js
@@ -3,9 +3,23 @@ import { Link } from 'react-router-dom';
 
 const Tarot = () => {
   return (
-    <main className="container mx-auto py-6">
-      <section>
-        <h2>TEST pages</h2>
+    <main className="container mx-auto py-6 flex flex-col grow">
+      <section className=" flex grow justify-center items-center">
+        <img
+          className="max-w-full w-[200px]"
+          src="/src/img/site/tarot-card-cover.svg"
+          alt="Crystal Ball"
+        />
+        <img
+          className="max-w-full w-[200px] mx-4"
+          src="/src/img/site/tarot-card-cover.svg"
+          alt="Crystal Ball"
+        />
+        <img
+          className="max-w-full w-[200px]"
+          src="/src/img/site/tarot-card-cover.svg"
+          alt="Crystal Ball"
+        />
       </section>
     </main>
   );
