028d06cf68f703cfac2552448154bcfaffce489a
John Lai <lai.john.py@gmail.com>
2023-04-01 14:31:39 -0400

    Finish style login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -55,7 +55,7 @@ function Login(props) {
                   name="email"
                   type="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
                   placeholder="name@company.com"
                   required=""
                   onChange={handleChange}
@@ -68,7 +68,7 @@ function Login(props) {
                   Password
                 </label>
                 <input
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 "
                   placeholder="******"
                   name="password"
                   type="password"
@@ -105,7 +105,7 @@ function Login(props) {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-purple-600 hover:bg-purple-700font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                className="w-full text-white bg-green-600 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                 Sign in
               </button>
               <p className="text-sm font-light text-gray-500 ">
