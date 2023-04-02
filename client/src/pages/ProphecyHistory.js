0273c4b6c5d4e8060d0f0e60f2e667d35fb7a6f7
Thomas Tran <tranthom618@gmail.com>
2023-04-02 00:52:31 -0400

    Current prophecy page - Errors still relating to 'Rendered more hooks than during prev render'

--- client/src/pages/ProphecyHistory.js
+++ client/src/pages/ProphecyHistory.js
@@ -1,39 +1,17 @@
-import React, { useState, useEffect } from "react";
+import * as React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_PROPHECY } from "../utils/queries";
+
+import { useTable } from "react-table";
 
 //upload photos
 
 function ProphecyHistory() {
+  // Grabbing query data from the MongoDB database
   const { loading, error, data } = useQuery(QUERY_PROPHECY);
-  
-  if (loading) return 'Loading...';
+
+  if (loading)return "Loading...";
   if (error) return `Error! ${error.message}`;
-
-  // let user;
-
-  // if (data) {
-  //   user = data.user;
-  //   console.log(user);
-  // }
-  
-  // Use GraphQL to get prophecyHistory, then use loop to go through each and display
-  // const [historyContainer, setHistory] = useState([]);
-
-  ////////////////////////////
-  // Testing for seeding
-    
-
-  let updatedProphecy = data.prophecyHistories.map((data) => {
-    console.log(data);
-    return data;
-  });
-    // const newHistorySeed = [data, ...historyContainer];
-    // console.log(newHistorySeed);
-
-    // // Call setBucket to update state with our new set of bucket list items
-    // setHistory(newHistorySeed);
-  ////////////////////////////
 
   // const removeHistoryItem = (id) => {
   //   const updatedHistory = [...historyContainer].filter(
@@ -42,14 +20,65 @@ function ProphecyHistory() {
 
   //   setHistory(updatedHistory);
   // };
+  const tableData = React.useMemo(() => data.prophecyHistories, [data]);
+  const columns = React.useMemo(() => [
+    {
+      Header: "Date",
+      accessor: "readingDate",
+    },
+    {
+      Header: "Cards",
+      accessor: "cards",
+    },
+    {
+      Header: "CardPosition",
+      accessor: "cardPosition",
+    },
+    {
+      Header: "ResultsAI",
+      accessor: "resultsAI",
+    },
+    {
+      Header: "UserName",
+      accessor: "user",
+    },
+  ],
+  []);
+
+  
+
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({ columns, tableData});
 
   return (
     <div>
       <h1>History of Prophecies</h1>
-      <div
-        // historyContainer={historyContainer}
-        // removeHistoryItem={removeHistoryItem}
-      ></div>
+      <div className="container">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
