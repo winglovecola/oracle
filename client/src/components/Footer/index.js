0892972e40db00ca7d6da5fdb099baf3b8e94a1e
John Lai <lai.john.py@gmail.com>
2023-04-02 17:26:34 -0400

    add background colour to body

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,10 +3,9 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-
     <div className="mt-auto text-center">
-      <h6>Project by Wing, Thomas & John</h6>
-      <ul className="text-xs">
+      <h6 className="text-xs">Project by Wing, Thomas & John</h6>
+      {/* <ul className="text-xs">
         <li>
           <a
             href="https://unsplash.com/photos/pXQncqqo1O4?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
@@ -14,7 +13,7 @@ function Footer() {
             Background Photo Unsplash
           </a>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
