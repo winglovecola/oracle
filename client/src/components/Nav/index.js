93182f6d4757de3e001cde8128a1d4c73a760681
John Lai <lai.john.py@gmail.com>
2023-04-02 20:25:45 -0400

    Add tooltip too logged out mode

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -10,12 +10,19 @@ function Nav() {
         <ul className="flex flex-wrap flex-row gap-3">
           <li className="">
             <Link to="/">
+              <div className="group flex relative">
+                <img
+                  className="max-w-full w-[30px] md:w-[53px]"
+                  src="/src/img/icons/tarot.png"
+                  alt="Tarot Game"
+                />
+                <span
+                  className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-xs text-gray-100 rounded-md absolute left-1/2
+    -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                  Tarot
+                </span>
+              </div>
               {/* Tarot */}
-              <img
-                className="max-w-full w-[30px] md:w-[53px]"
-                src="/src/img/icons/tarot.png"
-                alt=""
-              />
             </Link>
           </li>
           <li className="">
@@ -55,40 +62,70 @@ function Nav() {
           <li className="">
             <Link to="/">
               {/* Tarot */}
-              <img
-                className="max-w-full w-[30px] md:w-[53px]"
-                src="/src/img/icons/tarot.png"
-                alt=""
-              />
+              <div className="group flex relative">
+                <img
+                  className="max-w-full w-[30px] md:w-[53px]"
+                  src="/src/img/icons/tarot.png"
+                  alt="Tarot Game"
+                />
+                <span
+                  className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-xs text-gray-100 rounded-md absolute left-1/2
+    -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                  Tarot
+                </span>
+              </div>
             </Link>
           </li>
           <li className="">
             <Link to="/store">
               {/* Store */}
-              <img
-                className="max-w-full w-[30px] md:w-[53px]"
-                src="/src/img/icons/shop.png"
-                alt=""
-              />
+
+              <div className="group flex relative">
+                <img
+                  className="max-w-full w-[30px] md:w-[53px]"
+                  src="/src/img/icons/shop.png"
+                  alt="Shop"
+                />
+                <span
+                  className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-xs text-gray-100 rounded-md absolute left-1/2
+    -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                  Shop
+                </span>
+              </div>
             </Link>
           </li>
           <li className="">
             <Link to="/signup">
               {/* Signup */}
-              <img
-                className="max-w-full w-[30px] md:w-[53px]"
-                src="/src/img/icons/signup.png"
-                alt=""
-              />
+              <div className="group flex relative">
+                <img
+                  className="max-w-full w-[30px] md:w-[53px]"
+                  src="/src/img/icons/signup.png"
+                  alt="Sign Up"
+                />
+                <span
+                  className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-xs text-gray-100 rounded-md absolute left-1/2
+    -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                  Sign Up
+                </span>
+              </div>
             </Link>
           </li>
           <li className="">
             <Link to="/login">
-              <img
-                className="max-w-full w-[30px] md:w-[53px]"
-                src="/src/img/icons/login.png"
-                alt=""
-              />
+              <div className="group flex relative">
+                <img
+                  className="max-w-full w-[30px] md:w-[53px]"
+                  src="/src/img/icons/login.png"
+                  alt="Login"
+                />
+                <span
+                  className="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-xs text-gray-100 rounded-md absolute left-1/2
+    -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto">
+                  Login
+                </span>
+              </div>
+
               {/* Login */}
             </Link>
           </li>
